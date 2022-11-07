4dde5ef4e549b8a09ae7b0e47c7ebba049b5eab8
Osvaldo Brignoni <brignoni@users.noreply.github.com>
2022-11-06 20:10:01 -0600

    format with autopep8

--- ebook.py
+++ ebook.py
@@ -13,32 +13,25 @@ remove_tags = [
     'awsdocs-filter-selector',
 ]
 
-page_still_valid = True
-authors = set()
-page = 1
-
-# get html with request
-# get menu items
-# get body
 
 def get_link_content(link, state):
     link_response = requests.get(state['base_url'] + link['href'])
-    
-    soup = bs4.BeautifulSoup(link_response.content.decode('utf-8'),'lxml')
+
+    soup = bs4.BeautifulSoup(link_response.content.decode('utf-8'), 'lxml')
 
     body = soup.select_one('#main-col-body')
-    
+
     for remove_tag in remove_tags:
         for tag in body.find_all(remove_tag):
             tag.decompose()
-    
+
     html = re.sub(r'[\ \n]{2,}', ' ', str(body))
     html = re.sub(r'<p>[\ \n]{1,}', '<p>', html)
     html = re.sub(r'[\ \n]{1,}</p>', '</p>', html)
     html = re.sub(r'</ul>', '</ul>\n\n', html)
-    
+
     md = markdownify.markdownify(
-        html, 
+        html,
         heading_style=markdownify.ATX
     )
 
@@ -46,38 +39,37 @@ def get_link_content(link, state):
 
     prefix = str(state['count']).zfill(2) + '-'
 
-    html_file = open('.output_html/' + prefix + link['href'],'w+')
+    html_file = open('.output_html/' + prefix + link['href'], 'w+')
     html_file.write(html)
     html_file.close()
-    
-    md_file = open('.output_md/' + prefix + link['href'].replace('.html','.md'),'w+')
+
+    md_file = open('.output_md/' + prefix +
+                   link['href'].replace('.html', '.md'), 'w+')
     md_file.write(md)
     md_file.close()
-    
+
     state['count'] += 1
-    
+
     if 'contents' in link:
-        for sub_link in link['contents']:        
+        for sub_link in link['contents']:
             get_link_content(sub_link, state)
-    
+
     return True
 
-def main():    
+
+def main():
     url = sys.argv[1]
     state = {'count': 0, 'base_url': re.sub("/[\w+\-\_]*.html", "/", url)}
-    
-    print(state['base_url'])
-    
-    toc_response = requests.get(state['base_url'] + 'toc-contents.json')
-    
-    toc = json.loads(toc_response.text)
-    
-    # print(toc['contents'])
 
-    for link in toc['contents']:        
+    toc_response = requests.get(state['base_url'] + 'toc-contents.json')
+
+    toc = json.loads(toc_response.text)
+
+    for link in toc['contents']:
         get_link_content(link, state)
 
     return True
+
 
 if __name__ == "__main__":
     main()
